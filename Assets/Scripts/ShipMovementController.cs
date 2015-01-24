473f015444c9561a7c9c74f5f88999af4ed5256b
silvedko <silvedko@gmail.com>
2015-01-24 16:58:19 +0300

    [SI-002] Add movement script for spaceship

--- Assets/Scripts/ShipMovementController.cs
+++ Assets/Scripts/ShipMovementController.cs
@@ -1,15 +1,41 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+enum JoystickDirection : int 
+{
+	left = -1,
+	right = 1
+}
+
 public class ShipMovementController : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
+	[SerializeField]
+	Transform spaceShip;
+
+	[SerializeField]
+	JoystickDirection jDir;
+
+	bool shipCanMove = true;
+
+	void Start () 
+	{
+		
 	}
 	
-	// Update is called once per frame
-	void Update () {
-	
+	void OnMouseDown () 
+	{
+		InvokeRepeating ("Move",0.05f, 0.05f);
+	}
+
+	void OnMouseUp ()
+	{
+		CancelInvoke ();
+	}
+
+	void Move ()
+	{
+		spaceShip.localPosition = new Vector3 (spaceShip.localPosition.x + 0.1f * (int)jDir, 
+		                                       spaceShip.localPosition.y,
+		                                       spaceShip.localPosition.z);
 	}
 }
